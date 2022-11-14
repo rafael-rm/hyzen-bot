e4913a9c45d3984e62291cc32ef93cc18fdc32b4
rafael-rm <rafael.rmo@outlook.com>
2022-11-14 17:49:32 -0300

    Comando para exibir o ping da aplicação.

---
+++ src/commands/dev/ping.py
@@ -0,0 +1,22 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+
+class Ping(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'[INFO] Carregado arquivo: {__name__}')
+
+
+    @app_commands.command(name='ping', description='Latência da aplicação.')
+    async def ping(self, interaction: discord.Interaction): 
+        await interaction.response.send_message(f"A aplicação encontra-se com **{round(self.bot.latency * 1000)}ms** de latência") 
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(Ping(bot))
