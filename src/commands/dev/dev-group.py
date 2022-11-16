2f8549220369cb8d8d1985f4927b811171e3bc73
rafael-rm <rafael.rmo@outlook.com>
2022-11-16 19:41:13 -0300

    Pequenas alterações no grupo de comandos de dev.

--- src/commands/dev/dev-group.py
+++ src/commands/dev/dev-group.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 from src.database.firebase import FirebaseDB
 import psutil
 
-class Dev(commands.Cog):
+
+class DevCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
@@ -23,13 +24,15 @@ class Dev(commands.Cog):
         print(f'[INFO] Carregado arquivo: {__name__}')
 
 
+    # Comando de PING
     @group.command(name='ping', description='Mostra o ping da aplicação.')
     @permissao_usar_cmd()
     async def ping(self, interaction: discord.Interaction): 
         await FirebaseDB.contador_comandos(self.bot.database)
         await interaction.response.send_message(f"A aplicação encontra-se com **{round(self.bot.latency * 1000)}ms** de latência") 
 
-    
+
+    # Comando de RAM
     @group.command(name='ram', description='Mostra o uso de RAM do servidor.')
     @permissao_usar_cmd()
     async def ram(self, interaction: discord.Interaction):
@@ -37,6 +40,7 @@ class Dev(commands.Cog):
         await interaction.response.send_message(f"A máquina encontra-se utilizando **{psutil.virtual_memory().used / 1024 / 1024:.0f}/{psutil.virtual_memory().total / 1024 / 1024:.0f}MB ({psutil.virtual_memory().percent}%)** de RAM")
 
 
+    # Comando de CPU
     @group.command(name='cpu', description='Mostra o uso da CPU do servidor.')
     @permissao_usar_cmd()
     async def cpu(self, interaction: discord.Interaction):
@@ -44,6 +48,17 @@ class Dev(commands.Cog):
         await interaction.response.send_message(f"A máquina possui **{psutil.cpu_count()} núcleos lógicos** e está utilizando **{psutil.cpu_percent()}%** de sua capacidade total")
 
     
+    # Comando de SYNC
+    @group.command(name='sync', description='Sincroniza os comandos da aplicação.')
+    @permissao_usar_cmd()
+    async def sync(self, interaction: discord.Interaction):
+        await FirebaseDB.contador_comandos(self.bot.database)
+        await interaction.response.send_message('Sincronizando aplicação com o Discord...')
+        await self.bot.tree.sync()
+        await interaction.channel.send('Aplicação sincronizada com o Discord.')
+
+
+    # Erro do comando PING
     @ping.error
     async def ping_error(self, interaction: discord.Interaction, error):
         await FirebaseDB.contador_comandos(self.bot.database)
@@ -54,6 +69,7 @@ class Dev(commands.Cog):
             print(error)
 
 
+    # Erro do comando RAM
     @ram.error
     async def ram_error(self, interaction: discord.Interaction, error):
         await FirebaseDB.contador_comandos(self.bot.database)
@@ -64,6 +80,7 @@ class Dev(commands.Cog):
             print(error)
 
 
+    # Erro do comando CPU
     @cpu.error
     async def cpu_error(self, interaction: discord.Interaction, error):
         await FirebaseDB.contador_comandos(self.bot.database)
@@ -74,5 +91,16 @@ class Dev(commands.Cog):
             print(error)
 
 
+    # Erro do comando SYNC
+    @sync.error
+    async def sync_error(self, interaction: discord.Interaction, error):
+        await FirebaseDB.contador_comandos(self.bot.database)
+        if isinstance(error, app_commands.CheckFailure):
+            await interaction.response.send_message("Você não tem permissão para executar esse comando.", ephemeral=True)
+        else:
+            await interaction.response.send_message("Ocorreu um erro ao executar o comando.", ephemeral=True)
+            print(error)
+
+
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(Dev(bot))
+    await bot.add_cog(DevCommands(bot))
