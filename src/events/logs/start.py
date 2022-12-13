38fc67ac8dd6d58b6378d2b9c03a87fd17d20c79
rafael-rm <rafael.rmo@outlook.com>
2022-12-13 01:30:21 -0300

    Log/aviso de quando a aplicação foi iniciada.

---
+++ src/events/logs/start.py
@@ -0,0 +1,23 @@
+from discord.ext import commands
+import logging
+
+class StartLog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logging.info(f'Carregado: {__name__}')
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logging.info(f'Aplicação iniciada com sucesso em {self.bot.user.name}#{self.bot.user.discriminator} ({self.bot.user.id}).')
+        logging.info(f'Total de servidores: {len(self.bot.guilds)}')
+        print(f'[INFO] Aplicação iniciada com sucesso em {self.bot.user.name}#{self.bot.user.discriminator} ({self.bot.user.id}).')
+        print(f'[INFO] Total de servidores: {len(self.bot.guilds)}')
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(StartLog(bot))
